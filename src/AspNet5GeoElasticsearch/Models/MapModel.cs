8016f4743a827f3936e0ffeeb6ede2ceae6fcc30
damienbod <damien_bod@hotmail.com>
2016-02-01 19:13:27 +0100

    Adding some comments to test the response model in api doc

--- src/AspNet5GeoElasticsearch/Models/MapModel.cs
+++ src/AspNet5GeoElasticsearch/Models/MapModel.cs
@@ -2,9 +2,24 @@
 {
 	public class MapModel
 	{
+        /// <summary>
+        /// The Map Data comment yep
+        /// </summary>
 		public string MapData { get; set; }
+
+        /// <summary>
+        /// My Center Longitude
+        /// </summary>
 		public double CenterLongitude { get; set; }
+
+        /// <summary>
+        /// My Center Latitude
+        /// </summary>
 		public double CenterLatitude { get; set; }
+
+        /// <summary>
+        /// Max distance in meter...
+        /// </summary>
 		public uint MaxDistanceInMeter { get; set; }
 	}
 }
