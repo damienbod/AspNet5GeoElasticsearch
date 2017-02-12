e3cc0d6d7bbc97a510c44b37de6a76d5e882b8e1
damienbod <damien_bod@hotmail.com>
2017-02-12 10:04:25 +0100

    renaming namespaces

--- src/AspNetCoreGeoElasticsearch/Program.cs
+++ src/AspNetCoreGeoElasticsearch/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace AspNet5GeoElasticsearch
+namespace AspNetCoreGeoElasticsearch
 {
     /// <summary>
     /// main
